62b026c8e8ac6f44ba2f764b2fb054a278b2d72e
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-03-13 10:13:26 +0900

    [javaconfig]

---
+++ 01_lecture-source/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/MenuService.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.section02.javaconfig;
+
+import java.util.List;
+
+public class MenuService {
+    public List<MenuDTO> selectAllMenu() {
+    }
+
+    public MenuDTO selectMenuByCode(int code) {
+    }
+
+    public boolean registMenu(MenuDTO menu) {
+    }
+
+    public boolean modifyMenu(MenuDTO menu) {
+    }
+
+    public boolean deleteMenu(int code) {
+    }
+}
